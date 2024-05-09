42c718c15b40eaf68e1d0987ec8e6a41a77c1e9b
thanhdanh27600 <thanhdanh27600@gmail.com>
2024-05-09 10:24:59 +0700

    try fix gpg

--- scripts/decrypt_backend_secret.sh
+++ scripts/decrypt_backend_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd backend
-echo `which gpg`
+wget https://download.docker.com/linux/ubuntu/gpg
 
 if [ -z "$1" ] || [ -z "$2" ]
 then
@@ -9,7 +9,7 @@ else
     echo "Decrypt with host: $1, to $2...";
     # --batch to prevent interactive command
     # --yes to assume "yes" for questions
-    /usr/bin/gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+    ./gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
     --output $2 $1
     echo "Decrypt successfully";
 fi
