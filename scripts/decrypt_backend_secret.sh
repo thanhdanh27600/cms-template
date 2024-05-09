5e6351fc19940112fd9b02aaa694ecdf0630e5f9
thanhdanh27600 <thanhdanh27600@gmail.com>
2024-05-09 10:45:19 +0700

    revert

--- scripts/decrypt_backend_secret.sh
+++ scripts/decrypt_backend_secret.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 cd backend
-wget https://download.docker.com/linux/ubuntu/gpg
-chmod 777 gpg
 
 if [ -z "$1" ] || [ -z "$2" ]
 then
@@ -10,7 +8,7 @@ else
     echo "Decrypt with host: $1, to $2...";
     # --batch to prevent interactive command
     # --yes to assume "yes" for questions
-    ./gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+    gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
     --output $2 $1
     echo "Decrypt successfully";
 fi
